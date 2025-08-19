8d314a933e03a089e36fb8449af81a2f5af41135
ryan <ryxnole@gmail.com>
2025-08-19 11:51:52 +0330

    before changing directory

--- init.sh
+++ init.sh
@@ -4,13 +4,17 @@ qdbus6 org.kde.KWin /KWin org.kde.KWin.setCurrentDesktop 1 &
 sleep 1
 konsole -e 'pipes.sh -K -C -f 20 -t 3' &
 sleep 1
-konsole -e 'bpytop' &
+konsole -e 'tty-clock -c -C 7 -S -b -n -B -D' &
 sleep 1
 qdbus6 org.kde.KWin /KWin org.kde.KWin.setCurrentDesktop 2
 sleep 1
-konsole -e 'cava' &
-konsole -e 'tty-clock -c -C 7 -S -b -n -B -D' &
-spotify &
+konsole -e 'bpytop' &
 sleep 1
 qdbus6 org.kde.KWin /KWin org.kde.KWin.setCurrentDesktop 3
+sleep 1
+konsole -e 'cava' &
+sleep 1
+spotify &
+sleep 1
+qdbus6 org.kde.KWin /KWin org.kde.KWin.setCurrentDesktop 4
 hiddify
